544a63c49768988102bc7fb1b5a6714b5ebb51e7
imara abdullah <102338647+imaraabdullah0@users.noreply.github.com>
2022-08-27 19:11:04 +0200

    start-React-portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import {MenuIcon} from '@heroicons/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='text-center cursor-pointer items-center border p-4rem bg-slate-300 font-mono justify-between	font-[900] text-lg drop-shadow-md brightness-90	flex	z-10'>
+   <h2 className='text-3xl pl-[10px] font-bold mr-4rem text-white hover:text-blue-100'>PSYCHO CODING</h2>
+    <ul className=' hidden md:flex text-white flex text-sm  justify-between mr-[5px]'>
+      <li className='ml-[10px] hover:-translate-y-1 hover:scale-110 hover:text-blue-300'>Home</li>
+      <li className='ml-[10px] hover:-translate-y-1 hover:scale-110 hover:text-blue-300'>About</li>
+      <li className='ml-[10px] hover:-translate-y-1 hover:scale-110 hover:text-blue-300'>My Works</li>
+      <li className='ml-[10px] hover:-translate-y-1 hover:scale-110 hover:text-blue-300'>Contact</li>
+    </ul>
+    <div className='hidden md:flex pr-4'>
+       <button className='hover:-translate-y-1 hover:scale-110 hover:text-yellow-100 border-none text-sm bg-transparent mr-4 text-blue-400'>Sing in</button>
+       <button className='px-2 py-0.5 bg-orange-200 rounded hover:-translate-y-1 hover:bg-blue-100 hover:text-red-400 px-8 py-3 text-sm text-gray-500'>Sing up</button>
     </div>
+
+   </div>
   );
 }
 
